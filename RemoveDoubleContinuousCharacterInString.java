32c91613d1891a26cf3600e460a22ea88933c402
Suseendhiran Rv <rvsusee@gmail.com>
2022-09-30 14:34:29 +0530

    Add files via upload

---
+++ RemoveDoubleContinuousCharacterInString.java
@@ -0,0 +1,40 @@
+package rvsusee;
+
+public class RemoveDoubleContinuousCharacterInString {
+	public static void main(String[] args) {	
+		String str = "abbccbdabcoc";
+		while(true) {
+			if(checkRepeatOrNot(str)) {
+				str = removeRepeatedChar(str);
+			}else {
+				System.out.println("Output is: "+str);				
+				break;
+			}
+		}
+	}
+
+	static String removeRepeatedChar(String str) {
+		String temp = "";
+
+		for (int i = 0; i < str.length()-1; i++) {
+			if (str.charAt(i) == str.charAt(i+1)) {
+				i++;
+			}else {
+				temp+= str.charAt(i);
+			}
+		}
+		return temp;
+	}
+
+	static boolean checkRepeatOrNot(String str) {
+		for (int i = 0; i < str.length()-1; i++) {
+			for (int j = 1; j < str.length(); j++) {
+		
+				if (str.charAt(i) == str.charAt(j)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
