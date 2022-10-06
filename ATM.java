5ca3f096595589eb08551688e889a01a56e2cef1
Suseendhiran Rv <rvsusee@gmail.com>
2022-10-06 11:26:20 +0530

    Add files via upload

--- ATM.java
+++ ATM.java
@@ -1,13 +1,13 @@
-package rvsusee.atm;
+package oct;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ATM {
+	static int atmcash = 60000;
 	
-
-	public static void options() {
-		 Scanner sc=new Scanner(System.in);
+	public static void options() throws PinMissMatchException,CashNotFoundException {
+		 Scanner sc = new Scanner(System.in);
 
 		 while(true) {
 			 System.out.println("Enter your Pin Number: ");
@@ -24,7 +24,6 @@ public class ATM {
 					 }else if(option ==2) {
 						 withdraw(pinNumber);
 					 }else if (option == 3) {
-						 System.out.println("Fast Cash");
 						 fastCash(pinNumber);
 					 }else if(option ==4){
 						 System.out.println("Thank You!!!");
@@ -34,25 +33,57 @@ public class ATM {
 					}
 				 }	
 			 }else {
-				 System.out.println("PIN Number is Wrong!. Kindly Enter Valid Pin!");
-			 	}
+				 try {
+					 throw new PinMissMatchException("PIN Number is Wrong!. Kindly Enter Valid Pin!");
+				 }catch(PinMissMatchException ex) {
+//					 System.out.println(ex);
+				 }
+				}
 			 }
 		}
-	public static void withdraw(int pinNumber) {
+	public static void withdraw(int pinNumber) throws CashNotFoundException{
 		 Scanner sc=new Scanner(System.in);
 		 System.out.print("Enter withdrawal Amount: ");
 		 int withdrawalAmount=sc.nextInt();
-		 if(withdrawalAmount<= acc.get(pinNumber)) {
+		 if(withdrawalAmount<= acc.get(pinNumber) && withdrawalAmount<= atmcash) {
+			 atmcash = atmcash - withdrawalAmount ;
 			 System.out.println("Withdrawal Completed!");
 			 System.out.println("Withdrawal Amount: "+withdrawalAmount);
 			 acc.put(pinNumber, acc.get(pinNumber)-withdrawalAmount);
 			 balanceEnquire(pinNumber);
 		 }else {
-			 System.out.println("Insufficient Balance!");
-			 balanceEnquire(pinNumber);
+			 if (withdrawalAmount > acc.get(pinNumber)) {
+				 System.out.println("Insufficient Balance!");
+				 balanceEnquire(pinNumber);
+			 }else if(withdrawalAmount > atmcash) {
+				 try {
+					 throw new CashNotFoundException("Cash Not found");
+				 }catch(CashNotFoundException ex) {
+				 }
+			 }
 		 }
 	 }
-		 
+	public static void withdraw(int pinNumber, int withdrawalAmount) {
+		 if(withdrawalAmount<= acc.get(pinNumber) && withdrawalAmount <= atmcash) {
+			 atmcash = atmcash - withdrawalAmount ;
+			 System.out.println("Withdrawal Completed!");
+			 System.out.println("Withdrawal Amount: "+withdrawalAmount);
+			 
+			 acc.put(pinNumber, acc.get(pinNumber)-withdrawalAmount);
+			 balanceEnquire(pinNumber);
+		 }else {
+			 if (withdrawalAmount > acc.get(pinNumber)) {
+				 System.out.println("Insufficient Balance!");
+				 balanceEnquire(pinNumber);
+			 }else if(withdrawalAmount > atmcash) {
+				 try {
+					 throw new CashNotFoundException("Cash Not found");
+				 }catch(CashNotFoundException ex) {
+				 }
+			 }
+		 }
+	 }
+
 	public static void balanceEnquire(int pinNumber) {
 		 if(acc.containsKey(pinNumber)) {
 			 System.out.println("Your Pin Numeber: "+pinNumber+"\nCurrent Balance is: "+acc.get(pinNumber));
@@ -62,24 +93,41 @@ public class ATM {
 		 }
 	}
 		 
-		
 	public static void fastCash(int pinNumber) {
 		 double Amount = 0;
 		 Scanner sc=new Scanner(System.in);
-		 System.out.print("Enter your withdrawal Amount Here:");
-		 int withdrawalAmount=sc.nextInt();
-		 if(Amount<= acc.get(pinNumber)) {
-			
-		
+		 boolean flag = true;
+		 while(flag) {
+			 System.out.println("choose below option: ");
+			 System.out.println("1.$5,000 \t2.$10,000 \n3.$15,000 \t4.$20,000");
+			 int option = sc.nextInt();
+			 switch(option) {
+			 case 1: 
+				 withdraw(pinNumber,5000);
+				 flag = false;
+				 break;
+			 case 2: 
+				 withdraw(pinNumber,10000);
+				 flag = false;
+				 break;
+			 case 3: 
+				 withdraw(pinNumber,15000);
+				 flag = false;
+				 break;
+			 case 4: 
+				 withdraw(pinNumber,20000);
+				 flag = false;
+				 break;
+			 default:
+				 System.out.println("Option Not found!");
+			 }
 		 }
-		
 	}
 	
+	static Map<Integer,Double> acc = new HashMap<>();
 	
-	static Map<Integer, Double> acc = new HashMap<>();
-	
-	public static void main(String[] args) {
-		acc.put(1111, (double) 5000);
+	public static void main(String[] args) throws PinMissMatchException,CashNotFoundException {
+		acc.put(1111, (double) 60000);
 		acc.put(2222, (double) 2000);
 		acc.put(3333, (double) 2600);
 		acc.put(4444, (double) 6500);
@@ -87,4 +135,15 @@ public class ATM {
 		options();
 	}
 	
+}
+
+class PinMissMatchException extends Exception{
+	public PinMissMatchException(String str) {
+		System.out.println(str);
+	}	
+}
+class CashNotFoundException extends Exception{
+	public CashNotFoundException(String str) {
+		System.out.println(str);
+	}
 }
